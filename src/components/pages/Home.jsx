44b15dc4c0426f333355744981e5d3e82144be7f
nikollsds <nikollds2812@gmail.com>
2024-07-17 21:21:18 +0200

    EL HTML de home creado

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,21 @@
+
+
+export default function Home() {
+  return (
+    <div>
+      
+      <h1>Tu freelancer de confianza</h1>
+
+      <p>¿Que tipo de usuario eres?</p>
+
+
+      <button>Login Clientes</button>
+
+      <button>Login Freenlancers</button>
+
+    </div>
+  )
+}
+
+
+
