43ab3f6ad350c547c10b9978ce4062211e1d413f
nikollsds <nikollds2812@gmail.com>
2024-07-19 20:43:03 +0200

    Funcionalidad creada en el componente login

--- src/components/Login/MainLogin.jsx
+++ src/components/Login/MainLogin.jsx
@@ -1,8 +1,37 @@
 
+import { useState, useContext } from "react"
 import { Link } from "react-router-dom"
+import {userDataContext} from "../Context/userDataContext.jsx";
 
 
 function MainLogin({ userType }) {
+
+        const context = useContext(userDataContext)
+
+        const [empty, setEmpty] = useState("")
+
+    const handleClick = (ev) => {
+        ev.preventDefault();
+        if (userData) {
+            context.setUserData(userData)
+            
+                   } else {
+
+                    setEmpty("Por favor complete los campos")
+
+                   }
+    };
+
+   const [userData, setUserData] = useState({
+      username:"",
+      password:""
+    })
+console.log(context);
+    const handleChange = (ev) => {
+    const {name,value} = ev.target
+    setUserData({...userData,[name]: value})
+    
+    }
 
     return (
         <main className={userType}>
@@ -15,14 +44,15 @@ function MainLogin({ userType }) {
             </p>
 
             <form action="">
-                <input type="text" name="username" placeholder="Usuario" />
+                <input onChange={handleChange} type="text" name="username" placeholder="Usuario" />
 
-                <input type="password" name="password" placeholder="Contraseña" />
+                <input onChange={handleChange} type="password" name="password" placeholder="Contraseña" />
             </form>
+            <p> {empty}</p>
             {/* aquí hay que ponerle una funcionalidad y vrear más paginas No merece la pena añadirlo
             <p>Olvidaste tu contraseña</p>
             */}
-            <button>Acceder</button>
+            <button onClick={handleClick}>Acceder</button>
 
             <p>¿Todavia no tienes una cuenta?</p>
             <Link to={`/${userType}`}>
--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -22,7 +22,7 @@ export default function Home() {
         </button>
       </Link>
 
-    </div >
+    </div>
   )
 }
 
