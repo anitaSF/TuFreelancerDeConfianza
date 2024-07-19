af25d2f1edb7fde37e2df5904812761ce68b639f
nikollsds <nikollds2812@gmail.com>
2024-07-19 22:23:06 +0200

    HTML de details creado

--- src/components/pages/Details.jsx
+++ src/components/pages/Details.jsx
@@ -4,6 +4,19 @@ export default function Details() {
   return (
     <div>
       
+      <h1>Tu freelancer de confianza</h1>
+       
+       <h2>¿Que servicio estas buscando?</h2>
+
+       <p>Selecciona el tipo de profesional que mejor se adapte a tus necesidades</p>
+
+    <div>tu manitas de confianza</div> 
+    <div>tu cuidador de confianza</div> 
+    <div>tu profesor de confianza</div> 
+    <div>tu tecnico de confianza</div> 
+    <div>tu canguro de confianza</div> 
+    <div>tu auxiliar de confianza</div> 
+
     </div>
   )
 }
