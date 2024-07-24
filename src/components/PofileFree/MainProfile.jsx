cd457bc13c3464c184658373f9e9de8f028ab6fa
nikollsds <nikollds2812@gmail.com>
2024-07-24 22:26:54 +0200

    Se crea filtro

--- src/components/PofileFree/MainProfile.jsx
+++ src/components/PofileFree/MainProfile.jsx
@@ -1,6 +1,92 @@
+import { useContext, useEffect, useState } from "react"
+import { freelanceContext } from "../Context/frelanceContext";
+import { userDataContext } from "../Context/userDataContext";
+import { getRemoveFreelancer } from "../service/ApiFreelancers";
+
 export default function MainProfile() {
+    const context = useContext(freelanceContext)
+
+    const contextUser = useContext(userDataContext)
 
 
+    const [userFreelancerId, setUserFreelancerId] = useState ("")
+    const [userFreelancer, setUserFreelancer] = useState ([])
+
+    const [userListFreelancer, setUserListFreelancer] = useState ([
+        
+    ])
+
+   
+
+    const [confirm, setConfirm] = useState(false)
+
+    const handleDeleteProfile = (ev) => {
+
+    console.log(context.userFreelancer); 
+
+    confirmDeleteProfile();
+        
+    freelancerFilter();
+
+    getRemoveFreelancer(context.userFreelancer.id)
+
+       }
+       useEffect(() => {
+        if (context.listFreelancer) {
+            setUserListFreelancer([...context.listFreelancer]);
+        }
+    }, [context.listFreelancer]);
+
+       useEffect(() => {
+        if (userListFreelancer.length > 0) {
+            freelancerFilter();
+        }
+    }, [userListFreelancer]);
+    
+    const freelancerFilter = () => {
+        const userEmail = context.userFreelancer.email.trim().toLowerCase();
+        console.log("Email from localStorage:", typeof userEmail);
+    
+        if (userEmail) {
+            console.log("userListFreelancer:", userListFreelancer);
+    
+            // Normaliza los emails en userListFreelancer antes de la comparación
+            const filteredUser = userListFreelancer.filter((user)=> {
+                console.log(user.email.trim().toLowerCase());
+                console.log(userEmail);
+                return user.email.trim().toLowerCase() === userEmail});
+                
+            console.log("Filtered user:", filteredUser);
+    
+            setUserFreelancer(filteredUser);
+        } else {
+            console.warn("No se encontró el email en localStorage");
+        }
+    };
+
+    const confirmDeleteProfile = () => {
+        
+    
+        if( window.confirm("¿estas seguro que deseas eliminar tu perfil?")) {
+
+
+
+            console.log("PerfilEliminado");
+            
+            setConfirm(true);
+
+        } else { 
+
+            console.log("Cancelado");
+        }
+
+
+            
+
+
+
+    }
+      
     return (
         <main>
             <article>
@@ -32,10 +118,10 @@ export default function MainProfile() {
             </article>
             {/* los botones deberían ir en otro componente con su lógica */}
             <article>
-                <form action="">
-                    <button>Editar perfil</button>
-                    <button>Eliminar perfil</button>
-                </form>
+                <div>
+                    <button onClick={freelancerFilter}>Editar perfil</button>
+                    <button type="button" onClick={() => {handleDeleteProfile()}}>Eliminar perfil</button>
+                </div>
             </article>
         </main>
     )
