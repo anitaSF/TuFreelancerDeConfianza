9c3ffebdc907311b35270c73e5d39745426f7147
nikollsds <nikollds2812@gmail.com>
2024-07-25 14:50:53 +0200

    Tareas de los Freelancer

---
+++ src/components/pages/InfoFree.jsx
@@ -0,0 +1,65 @@
+export default function InfoFree() {
+  return (
+    <div>
+      <article>
+        <h2>Tus manitas de confianza</h2>
+
+        <p> Montaje de muebles 
+            Fontanería 
+            Electricidad </p>
+
+      </article>
+
+      <article>
+        <h2>Tu cuidador de confianza</h2>
+
+        <p> Cuidar a los perros media jornada
+            Sacarlos a pasear durante esa jornada dos veces 
+            Al finalizar la jornada dejarles su alimento
+            
+        </p>
+        
+      </article>
+
+      <article>
+        <h2>Tu profesor de confianza</h2>
+
+        <p> Reforzar conocimientos en aquellas areas que sean de menor compresion para el alumno
+            Apoyar en los deberes correspondientes 
+         </p>
+        
+      </article>
+
+      <article>
+        <h2>Tu confianza en ti</h2>
+
+        <p>Brindamos apoyo en nuestras areas de conocimientos
+           Te ayudamos a solucionar tus problemas informaticos
+         </p>
+        
+      </article>
+
+      <article>
+        <h2>Tu canguro de confianza</h2>
+
+        <p> Cuidar al niño/a durante el tiempo requerido
+            Recogerlo al colegio
+            Acompañarlo a las actividades extraescolares
+
+        </p>
+        
+      </article>
+
+      <article>
+        <h2>Tu auxiliar de confianza</h2>
+           
+        <p> Brindamos apoyo en instalaciones de aplicaciones informaticas
+            Te acompañamos a tus diligencias si tienes alguna dificultad en trasladarse solos/as 
+            
+        </p>
+        
+      </article>
+
+    </div>
+  );
+}
